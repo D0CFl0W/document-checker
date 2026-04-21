d3f2541ac8982d5b25ba7c3db84296d3fd06712b
gribolet <esen@MacBook-Air-esen.local>
2026-04-22 03:24:11 +0500

    start

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
 from fastapi import FastAPI, HTTPException, Depends, Request
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
-from sqlalchemy.orm import Session
-from routers import students_checker
-from models import sql_users
-from databases import sqlite_users_service
 
 app = FastAPI(
     title="Анализатор документов",
@@ -14,15 +10,8 @@ app = FastAPI(
 
 app.mount('/static', StaticFiles(directory='static'), 'static')
 templates = Jinja2Templates(directory='templates')
-sql_users.sqlite_users_service.Base.metadata.create_all(bind=sqlite_users_service.engine)
 
-app.include_router(students_checker.router)
 
 @app.get("/")
 def return_basic(request: Request):
     return templates.TemplateResponse(name='place_holder.html', context={'request': request})
-
-@app.get("/users/")
-async def read_users(db: Session = Depends(sqlite_users_service.get_db)):
-    users = db.query(sql_users.User).all()
-    return users
--- models/sql_users.py
+++
@@ -1,9 +0,0 @@
-from databases import sqlite_users_service
-from sqlalchemy import  Column, Integer, String
-
-class User(sqlite_users_service.Base):
-    __tablename__ = "Users"
-
-    id = Column(Integer, primary_key=True, index=True)
-    login = Column(String, unique=True, index=True)
-    password = Column(String)
